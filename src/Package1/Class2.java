62c88c4fec6bde3c0c32b419ec864604e63b4d3e
VinodhiniPushparaj <62499318+VinodhiniPushparaj@users.noreply.github.com>
2020-08-06 16:49:08 +0530

    Update Class2.java

--- src/Package1/Class2.java
+++ src/Package1/Class2.java
@@ -5,6 +5,7 @@ public class Class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Class2");
+		System.out.println("From cloud to eclipse");
 
 	}
 
